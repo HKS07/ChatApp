294da792da970c986003606214e477336ec215f1
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-04 22:05:19 +0530

    Created whole Setting section UI

--- ChatApp-Front/src/components/MiddleSection/SettingsSection.jsx
+++ ChatApp-Front/src/components/MiddleSection/SettingsSection.jsx
@@ -1,4 +1,24 @@
 import { IoIosSearch } from "react-icons/io";
+import ProfileImg from "../../assets/Profile.png";
+import { IoIosLogOut } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { MdLockOutline } from "react-icons/md";
+import { BiMessageDetail } from "react-icons/bi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+
+const SettingsLabel = ({children,title}) => {
+  const textColor = title === 'Log out' ? "text-red-500" : "text-customWhite";
+  return (
+    <div className="flex hover:bg-customDarkWhite2">
+      <div className={`text-2xl ${textColor} m-5`}>
+        {children}
+      </div>
+      <div className={`flex-grow flex items-center text-lg ${textColor} border-b border-customDarkWhite2`}>
+        <p>{title}</p>
+      </div>
+    </div>
+  );
+};
 
 const SettingsSection = () => {
   return (
@@ -13,9 +33,20 @@ const SettingsSection = () => {
           placeholder="Search settings"
         />
       </div>
-      <div>
-        
+      <div className="flex">
+        <div className="mx-3 my-2">
+          <img src={ProfileImg} className="w-24 rounded-full" />
+        </div>
+        <div className="w-full flex flex-col justify-center ">
+          <p className="mr-3 text-customLightWhite">HKS</p>
+          <p className="mr-3 text-customDarkWhite">KuchBhi</p>
+        </div>
       </div>
+      <SettingsLabel title={"Account"}><CgProfile/></SettingsLabel>
+      <SettingsLabel title={"Privacy"}><MdLockOutline/></SettingsLabel>
+      <SettingsLabel title={"Chats"}><BiMessageDetail/></SettingsLabel>
+      <SettingsLabel title={"Notifications"}><IoMdNotificationsOutline/></SettingsLabel>
+      <SettingsLabel title={"Log out"}><IoIosLogOut/></SettingsLabel>
     </div>
   );
 };
