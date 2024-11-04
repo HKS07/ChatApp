e92eed2cc9e81da988c38d8272f075935365bf6d
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-04 23:22:00 +0530

    width updated

--- ChatApp-Front/src/components/RightSection/Body.jsx
+++ ChatApp-Front/src/components/RightSection/Body.jsx
@@ -4,7 +4,7 @@ const Body = () => {
   const lorem =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
   return (
-    <div className="flex-grow max-w-[950px] bg-message-body text-customLightWhite">
+    <div className="flex-grow w-full bg-message-body text-customLightWhite">
       <div className="flex flex-col  h-[640px] overflow-y-scroll custom-scrollbar">
         <MessageBox
           message={lorem}
