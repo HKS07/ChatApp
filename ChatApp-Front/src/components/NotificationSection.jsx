87620f78204a87d65777809857972959c13c7eed
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-05 08:54:11 +0530

    Added tooltip on the notification bar buttons

--- ChatApp-Front/src/components/NotificationSection.jsx
+++ ChatApp-Front/src/components/NotificationSection.jsx
@@ -1,34 +1,72 @@
 import { BiMessageDetail } from "react-icons/bi";
 import { IoSettingsOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
-
+import Banner from "./Utils/Banner";
+import { useState } from "react";
 const NotificationSection = ({ activeComponent, onComponentChange }) => {
+  const [currentHovered, setCurrentHovered] = useState("x");
   return (
     <div className="flex flex-col justify-between py-4 w-16 bg-customGray">
-      <div className="text-white text-2xl mx-auto">
+      <div
+        className="flex items-center text-white text-2xl mx-auto"
+        onMouseEnter={() => setCurrentHovered("Chats")}
+        onMouseLeave={() => setCurrentHovered("x")}
+      >
         {activeComponent === "ChatSection" ? (
           <div className="p-2 rounded-full bg-customDarkWhite">
             <BiMessageDetail />
           </div>
         ) : (
-          <div className="p-2" onClick={() => onComponentChange('ChatSection')}> <BiMessageDetail/></div>
+          <div className="p-2" onClick={() => onComponentChange("ChatSection")}>
+            {" "}
+            <BiMessageDetail />
+          </div>
         )}
+        {currentHovered === "Chats" && <Banner title={"Chats"} />}
       </div>
+
       <div className="text-white text-2xl mx-auto">
-      {activeComponent === "SettingsSection" ? (
-          <div className="p-2 rounded-full bg-customDarkWhite">
-            <IoSettingsOutline />
-          </div>
-        ) : (
-          <div className="p-2" onClick={() => onComponentChange('SettingsSection')}> <IoSettingsOutline/></div>
-        )}
-        {activeComponent === "ProfileSection" ? (
-          <div className="p-2 rounded-full bg-customDarkWhite">
-            <CgProfile />
-          </div>
-        ) : (
-          <div className="p-2" onClick={() => onComponentChange('ProfileSection')}> <CgProfile/></div>
-        )}
+        <div
+          className="flex items-center text-white text-2xl mx-auto"
+          onMouseEnter={() => setCurrentHovered("Settings")}
+          onMouseLeave={() => setCurrentHovered("x")}
+        >
+          {activeComponent === "SettingsSection" ? (
+            <div className="p-2 rounded-full bg-customDarkWhite">
+              <IoSettingsOutline />
+            </div>
+          ) : (
+            <div
+              className="p-2"
+              onClick={() => onComponentChange("SettingsSection")}
+            >
+              {" "}
+              <IoSettingsOutline />
+            </div>
+          )}
+          {currentHovered === "Settings" && <Banner title={"Settings"} />}
+        </div>
+
+        <div
+          className="flex items-center text-white text-2xl mx-auto"
+          onMouseEnter={() => setCurrentHovered("Profile")}
+          onMouseLeave={() => setCurrentHovered("x")}
+        >
+          {activeComponent === "ProfileSection" ? (
+            <div className="p-2 rounded-full bg-customDarkWhite">
+              <CgProfile />
+            </div>
+          ) : (
+            <div
+              className="p-2"
+              onClick={() => onComponentChange("ProfileSection")}
+            >
+              {" "}
+              <CgProfile />
+            </div>
+          )}
+          {currentHovered === "Profile" && <Banner title={"Profile"} />}
+        </div>
       </div>
     </div>
   );
--- ChatApp-Front/src/components/Utils/Banner.jsx
+++ ChatApp-Front/src/components/Utils/Banner.jsx
@@ -1,10 +1,9 @@
-
-const Banner = () => {
-    return (
-        <div >
-            <div className="bg-white text-black rounded-md">Chats</div>
-        </div>
-    )
-}
+const Banner = (props) => {
+  return (
+    <div className="absolute z-10 left-20 text-sm p-0 m-0">
+      <div className="bg-white text-black rounded-2xl p-2 shadow-lg">{props.title}</div>
+    </div>
+  );
+};
 
 export default Banner;
