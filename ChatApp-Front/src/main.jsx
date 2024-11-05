7169051acb2b4b57c1c6a17b4b49729640413e48
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-05 22:56:41 +0530

    Added Global Context

--- ChatApp-Front/src/main.jsx
+++ ChatApp-Front/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import {GlobalProvider} from "./components/Utils/GlobalContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GlobalProvider><App/></GlobalProvider>
   </StrictMode>
 );
