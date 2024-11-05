872a3af9898a7a46fa1dc609cf95193aee1d83f2
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-05 20:33:29 +0530

    Chats button category are updated

--- ChatApp-Front/src/components/MiddleSection/ChatSection.jsx
+++ ChatApp-Front/src/components/MiddleSection/ChatSection.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import UserLable from "./UsersLabel";
 
 const ChatSection = () => {
+  const [category, setCategory] = useState("All");
+  const handleCategory = (cat) => {
+    setCategory(cat);
+  };
   return (
     <div className="w-[446px] bg-customBlack text-white">
       <div className="text-2xl font-bold mx-5 my-2 py-2">Chats</div>
@@ -15,13 +20,32 @@ const ChatSection = () => {
         />
       </div>
       <div className="flex px-1 py-2 mx-2 mt-1">
-        <div className="mr-2 px-2 py-1 rounded-xl bg-customLightGray text-customDarkWhite">
+        <div
+          onClick={() => handleCategory("All")}
+          className={`mr-2 px-2 py-1 rounded-xl cursor-pointer ${
+            category == "All"
+              ? "bg-customGreen text-customGreen3"
+              : "bg-customLightGray text-customDarkWhite"
+          }`}
+        >
           All
         </div>
-        <div className="mr-2 px-2 py-1 rounded-xl bg-customLightGray text-customDarkWhite">
+        <div
+          onClick={() => handleCategory("Unread")}
+          className={`mr-2 px-2 py-1 rounded-xl cursor-pointer ${
+            category == "Unread"
+              ? "bg-customGreen text-customGreen3"
+              : "bg-customLightGray text-customDarkWhite"
+          }`}
+        >
           Unread
         </div>
-        <div className="mr-2 px-2 py-1 rounded-xl bg-customLightGray text-customDarkWhite">
+        <div onClick={() => handleCategory("Favourites")}
+          className={`mr-2 px-2 py-1 rounded-xl cursor-pointer ${
+            category == "Favourites"
+              ? "bg-customGreen text-customGreen3"
+              : "bg-customLightGray text-customDarkWhite"
+          }`}>
           Favourites
         </div>
       </div>
