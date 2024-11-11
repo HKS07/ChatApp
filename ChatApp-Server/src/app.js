49facc1855125b4a22f4c457ee5db8d6218a92c9
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-11 20:18:52 +0530

    Added routes to the app.js

--- ChatApp-Server/src/app.js
+++ ChatApp-Server/src/app.js
@@ -1,13 +1,26 @@
-// require('dotenv').config();
-const loadEnv = require('./utils/loadEnv')
-loadEnv();
-const express = require('express');
+require("dotenv").config();
+const { chatsRoutes, contactsRoutes, profileRoutes, messagesRoutes } = require('./routes/index');
+const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 3000;
-app.get('/',(req,res) => {
-    res.send('hello world')
-})
 
-app.listen(port,() => {
-    console.log(`Server listening on port ${port}`);
+app.use(bodyParser.json());  // Use bodyParser to handle JSON requests
+app.use(router);  // Mount the router
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+// Use the routes with their respective paths
+app.use('/chat', chatsRoutes);
+app.use('/contacts', contactsRoutes);
+app.use('/profile', profileRoutes);
+app.use('/message', messagesRoutes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
