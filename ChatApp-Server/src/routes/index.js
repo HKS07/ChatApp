17c8427e66ed49768e78ad5032ef7367b846dd5f
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-11 20:18:29 +0530

    modularized all the routes in index

---
+++ ChatApp-Server/src/routes/index.js
@@ -0,0 +1,12 @@
+// routes/index.js
+const chatsRoutes = require('./chats');
+const contactsRoutes = require('./contacts');
+const messagesRoutes = require('./messages');
+const profileRoutes = require('./profile')
+
+module.exports = {
+  chatsRoutes,
+  contactsRoutes,
+  messagesRoutes,
+  profileRoutes
+};
