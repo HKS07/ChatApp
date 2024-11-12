f778a1d9635ad2c032a49818d39826d80ac2912b
HKS07 <77383317+HKS07@users.noreply.github.com>
2024-11-12 23:14:32 +0530

    added getAllContact and delete contact controller

--- ChatApp-Server/src/controllers/contactsController.js
+++ ChatApp-Server/src/controllers/contactsController.js
@@ -1,39 +1,79 @@
-const {PrismaClient} = require('@prisma/client')
-const Prisma = new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-exports.getAllContacts = (req,res) => {
+exports.getAllContacts = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const user = await prisma.profiles.findUnique({
+      where: {
+        id: userId,
+      },
+    });
 
-}
+    res.status(200).json({ contacts: user.contacts });
+  } catch (error) {
+    console.log("error fectching users all contacts", error);
+    res.status(400).send("Bad request");
+  }
+};
 
-exports.getContact = (req,res) => {
+exports.getContact = (req, res) => {};
 
-}
+exports.addContact = async (req, res) => {
+  const { contactId, userId } = req.body;
+  try {
+    await prisma.profiles.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        contacts: {
+          push: contactId,
+        },
+      },
+    });
+    res.status(200).send("contact added successfully.");
+  } catch (error) {
+    console.log("error occured during adding contact to usser: ", error);
 
-exports.addContact = async (req,res) => {
-    const {contactId,userId} = req.body;
-    try {
-            await Prisma.profiles.update({
-                where: {
-                    id: userId
-                },
-                data: {
-                    contacts:{
-                        push: contactId
-                    }
-                }
-        })
-        res.status(200).send('contact added successfully.');
-    } catch (error) {
-        console.log("error occured during adding contact to usser: ",error);
-        
-        res.status(500).send('An error occured during adding new contact to user');
+    res.status(500).send("An error occured during adding new contact to user");
+  }
+};
+
+exports.deleteContact = async (req, res) => {
+  const { userId, contactId } = req.body;
+  try {
+    const user = await prisma.profiles.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      res.status(404).json({ message: "User doenst exist" });
+      return;
     }
-}
 
-exports.deleteContact = (req,res) => {
+    const updatedContacts = user.contacts.filter((curId) => curId != contactId);
+    console.log(updatedContacts);
 
-}
+    await prisma.profiles.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        contacts: {
+          set: updatedContacts,
+        },
+      },
+    });
 
-exports.updateContact = (req,res) => {
+    res.status(200).json({ message: "successfully deleted user contact" });
+  } catch (error) {
+    console.log("error deleting the user", error);
+    res
+      .status(500)
+      .json({ message: "not able to delete the contact from user." });
+  }
+};
 
-}
+exports.updateContact = (req, res) => {};
